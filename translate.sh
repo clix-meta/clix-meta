dcd660fc1ee78131d756d658340ff11ef1b8b901
Lars Barring <lars.barring@smhi.se>
2019-05-14 15:02:15 +0200

    Added column for standard name and updated some proposed standard names (frequency agnostic)

--- translate.sh
+++ translate.sh
@@ -1,2 +1,2 @@
-python jsonFlat2Hierarchial.py translationRules.json < climate_indices_CV20.json > climate_indices_CV20_DEF.json 2>errors.txt
-python listMissingItems.py < climate_indices_CV20_DEF.json > climate_indices_CV20_DEF_missing.txt
+python jsonFlat2Hierarchial.py translationRules.json < climate_indices_CV21.json > climate_indices_CV21_DEF.json 2>errors.txt
+python listMissingItems.py < climate_indices_CV21_DEF.json > climate_indices_CV21_DEF_missing.txt
