5846253648e1d538464669dfa05168996d145ff9
Lars Barring <lars.barring@smhi.se>
2019-11-05 16:31:38 +0100

    changed index function degreesum to temperaturesum

--- translate.sh
+++ translate.sh
@@ -1,2 +1,2 @@
-python jsonFlat2Hierarchial.py translationRules.json < climate_indices_CV21.json > climate_indices_CV21_DEF.json 2>errors.txt
-python listMissingItems.py < climate_indices_CV21_DEF.json > climate_indices_CV21_DEF_missing.txt
+python jsonFlat2Hierarchial.py translationRules.json < climate_indices_CV22.json > climate_indices_CV22_DEF.json 2>errors.txt
+python listMissingItems.py < climate_indices_CV22_DEF.json > climate_indices_CV22_DEF_missing.txt
